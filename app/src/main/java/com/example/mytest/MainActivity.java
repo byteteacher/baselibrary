e875e2c7102c6bd1fa132ba91d431a93f86c9649
zhangchaojun <zhangcj@bjhzwq.com>
2024-02-01 15:05:20 +0800

    1、主分支测试

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.bt_test_64:
 
+                //主分支测试
                 startActivity(new Intent(this,SecondActivity.class));
 
                 break;
